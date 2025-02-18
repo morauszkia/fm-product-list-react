05c2a89b22fd22e11fa183ccca09039b737d8bad
mantis <morauszki.andras@gmail.com>
2025-02-18 22:21:43 +0100

    ProductCard mobile styles

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,26 +1,36 @@
 import { getImageURL } from "../../util/image-util";
+import addToCartImg from "../../assets/images/icon-add-to-cart.svg";
+
+import classes from "./ProductCard.module.css";
 
 export default function ProductCard() {
   return (
     <li>
-      <picture>
-        <source
-          media="(min-width: 600px) and (max-width: 799px)"
-          srcSet={getImageURL("image-waffle-tablet.jpg")}
-        />
-        <source
-          media="(min-width: 800px)"
-          srcSet={getImageURL("image-waffle-desktop.jpg")}
-        />
-        <img
-          src={getImageURL("image-waffle-mobile.jpg")}
-          alt="Waffle with Berries"
-        />
-      </picture>
-      <button>Add to Cart</button>
-      <p>Waffle</p>
-      <p>Waffle with Berries</p>
-      <p>$6.50</p>
+      <div className={classes["img-container"]}>
+        <picture className={classes.img}>
+          <source
+            media="(min-width: 600px) and (max-width: 799px)"
+            srcSet={getImageURL("image-waffle-tablet.jpg")}
+          />
+          <source
+            media="(min-width: 800px)"
+            srcSet={getImageURL("image-waffle-desktop.jpg")}
+          />
+          <img
+            src={getImageURL("image-waffle-mobile.jpg")}
+            alt="Waffle with Berries"
+          />
+        </picture>
+        <button className={classes["btn--add"]}>
+          <img src={addToCartImg} alt="" />
+          Add to Cart
+        </button>
+      </div>
+      <div className={classes["card--text"]}>
+        <p className={classes.category}>Waffle</p>
+        <p className={classes.name}>Waffle with Berries</p>
+        <p className={classes.price}>$6.50</p>
+      </div>
     </li>
   );
 }
