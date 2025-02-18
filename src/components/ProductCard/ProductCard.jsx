32ab8887ad32046c607a421f8a05a527828f642e
András Morauszki <morauszki.andras@gmail.com>
2025-02-18 15:44:43 +0100

    ProductCard basic html structure

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,3 +1,26 @@
+import { getImageURL } from "../../util/image-util";
+
 export default function ProductCard() {
-  return <div>ProductCard</div>;
+  return (
+    <li>
+      <picture>
+        <source
+          media="(min-width: 600px) and (max-width: 799px)"
+          srcSet={getImageURL("image-waffle-tablet.jpg")}
+        />
+        <source
+          media="(min-width: 800px)"
+          srcSet={getImageURL("image-waffle-desktop.jpg")}
+        />
+        <img
+          src={getImageURL("image-waffle-mobile.jpg")}
+          alt="Waffle with Berries"
+        />
+      </picture>
+      <button>Add to Cart</button>
+      <p>Waffle</p>
+      <p>Waffle with Berries</p>
+      <p>$6.50</p>
+    </li>
+  );
 }
