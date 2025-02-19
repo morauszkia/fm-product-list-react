1775805254c4e9f06318236dec4e055abdde1739
mantis <morauszki.andras@gmail.com>
2025-02-19 21:19:19 +0100

    Empty cart styles

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,15 @@
 import CakeImg from "../../assets/images/illustration-empty-cart.svg";
 
+import classes from "./Cart.module.css";
+
 export default function Cart() {
   return (
-    <section>
-      <h2>Your Cart (0)</h2>
-      <img src={CakeImg} alt="Cake" />
-      <p>Your added items will appear here</p>
+    <section className={classes.cart}>
+      <h2 className={classes.title}>Your Cart (0)</h2>
+      <div className={classes.content}>
+        <img src={CakeImg} alt="Cake" className={classes.img} />
+        <p className={classes.text}>Your added items will appear here</p>
+      </div>
     </section>
   );
 }
