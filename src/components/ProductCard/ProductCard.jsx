a6c97437aa020519a5748cf3b4be282864c2460a
mantis <morauszki.andras@gmail.com>
2025-02-19 16:21:56 +0100

    Render product cards dynamically: use json data

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,25 +1,24 @@
+import propTypes from "prop-types";
+
 import { getImageURL } from "../../util/image-util";
 import addToCartImg from "../../assets/images/icon-add-to-cart.svg";
 
 import classes from "./ProductCard.module.css";
 
-export default function ProductCard() {
+export default function ProductCard({ image, name, category, price }) {
   return (
     <li>
       <div className={classes["img-container"]}>
         <picture className={classes.img}>
           <source
             media="(min-width: 600px) and (max-width: 799px)"
-            srcSet={getImageURL("image-waffle-tablet.jpg")}
+            srcSet={getImageURL(image.tablet)}
           />
           <source
             media="(min-width: 800px)"
-            srcSet={getImageURL("image-waffle-desktop.jpg")}
+            srcSet={getImageURL(image.desktop)}
           />
-          <img
-            src={getImageURL("image-waffle-mobile.jpg")}
-            alt="Waffle with Berries"
-          />
+          <img src={getImageURL(image.mobile)} alt="Waffle with Berries" />
         </picture>
         <button className={classes["btn--add"]}>
           <img src={addToCartImg} alt="" />
@@ -27,10 +26,17 @@ export default function ProductCard() {
         </button>
       </div>
       <div className={classes["card--text"]}>
-        <p className={classes.category}>Waffle</p>
-        <p className={classes.name}>Waffle with Berries</p>
-        <p className={classes.price}>$6.50</p>
+        <p className={classes.category}>{category}</p>
+        <p className={classes.name}>{name}</p>
+        <p className={classes.price}>${price.toFixed(2)}</p>
       </div>
     </li>
   );
 }
+
+ProductCard.propTypes = {
+  name: propTypes.string.isRequired,
+  image: propTypes.object.isRequired,
+  category: propTypes.string.isRequired,
+  price: propTypes.number.isRequired,
+};
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -2,16 +2,16 @@ import ProductCard from "../ProductCard/ProductCard";
 
 import classes from "./ProductList.module.css";
 
+import dessertsData from "../../data/data.json";
+
 export default function ProductList() {
   return (
     <main>
       <h1 className={classes.title}>Desserts</h1>
       <ul className={classes.grid}>
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        {dessertsData.map((dessert) => (
+          <ProductCard key={dessert.name} {...dessert} />
+        ))}
       </ul>
     </main>
   );
