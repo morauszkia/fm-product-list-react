2afe52ba7f1a39e55f7c34ceb459ef955fc1b387
mantis <morauszki.andras@gmail.com>
2025-02-21 12:14:23 +0100

    Style CartItems

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -2,17 +2,20 @@ import propTypes from "prop-types";
 
 import RemoveIcon from "../../assets/images/icon-remove-item.svg";
 
+import classes from "./CartItem.module.css";
+
 export default function CartItem({ name, price, amount }) {
   return (
-    <li>
-      <div>
-        <p>{name}</p>
-        <p>
-          <span>{amount}x</span> <span>@ ${price.toFixed(2)}</span>{" "}
-          <span>${(amount * price).toFixed(2)}</span>
+    <li className={classes.row}>
+      <div className={classes.text}>
+        <p className={classes.name}>{name}</p>
+        <p className={classes.calculation}>
+          <span className={classes.amount}>{amount}x&nbsp;</span>{" "}
+          <span className={classes.price}>@ ${price.toFixed(2)}</span>{" "}
+          <span className={classes.total}>${(amount * price).toFixed(2)}</span>
         </p>
       </div>
-      <button>
+      <button className={classes.remove}>
         <img src={RemoveIcon} alt="Remove icon" />
       </button>
     </li>
