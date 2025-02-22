29b9708ce48140c548350a294936cc2aadf9f34f
mantis <morauszki.andras@gmail.com>
2025-02-22 20:15:47 +0100

    RemoveIcon as React Component

--- src/components/CartItem/CartItem.jsx
+++ src/components/CartItem/CartItem.jsx
@@ -1,6 +1,6 @@
 import propTypes from "prop-types";
 
-import removeIcon from "../../assets/images/icon-remove-item.svg";
+import RemoveIcon from "../../assets/images/icon-remove-item.svg?react";
 
 import classes from "./CartItem.module.css";
 
@@ -16,7 +16,8 @@ export default function CartItem({ name, price, amount }) {
         </p>
       </div>
       <button className={classes.remove}>
-        <img src={removeIcon} alt="Remove icon" />
+        {/* <img src={removeIcon} alt="Remove icon" /> */}
+        <RemoveIcon />
       </button>
     </li>
   );
