9eddb0f5ac707d86413b3dfc8b5de1e28c1a5dd8
András Morauszki <morauszki.andras@gmail.com>
2025-02-27 15:21:21 +0100

    ConfirmationModal render to DOM

--- src/components/CartContent/CartContent.jsx
+++ src/components/CartContent/CartContent.jsx
@@ -1,16 +1,26 @@
+import { useState } from "react";
+import { createPortal } from "react-dom";
 import propTypes from "prop-types";
 
 import cakeImg from "../../assets/images/illustration-empty-cart.svg";
 import treeImg from "../../assets/images/icon-carbon-neutral.svg";
 
-import classes from "./CartContent.module.css";
 import CartContentList from "../CartContentList/CartContentList";
 import Button from "../Button/Button";
+import ConfirmationModal from "../ConfirmationModal/ConfirmationModal";
+
+import classes from "./CartContent.module.css";
 
 function CartContent({ content, onRemove }) {
+  const [modalOpen, setModalOpen] = useState(false);
+
   const totalSum = content
     .reduce((sum, item) => sum + item.amount * item.price, 0)
     .toFixed(2);
+
+  const startNewOrder = () => {
+    setModalOpen(false);
+  };
 
   return content.length ? (
     <div className={classes.content}>
@@ -26,7 +36,14 @@ function CartContent({ content, onRemove }) {
           This is a <strong>carbon-neutral</strong> delivery
         </p>
       </div>
-      <Button className={classes.btn}>Confirm Order</Button>
+      <Button className={classes.btn} onClick={() => setModalOpen(true)}>
+        Confirm Order
+      </Button>
+      {modalOpen &&
+        createPortal(
+          <ConfirmationModal cart={content} onButtonClick={startNewOrder} />,
+          document.getElementById("modal")
+        )}
     </div>
   ) : (
     <div className={classes.empty}>
