ea708bb50ff73673b7e5eab47c3a84344756edd1
András Morauszki <morauszki.andras@gmail.com>
2025-02-27 15:20:56 +0100

    ConfirmationModal styling

--- src/components/ConfirmationModal/ConfirmationModal.jsx
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,18 +1,23 @@
 import propTypes from "prop-types";
 
 import Button from "../Button/Button";
-
 import confirmIcon from "../../assets/images/icon-order-confirmed.svg";
+
+import classes from "./ConfirmationModal.module.css";
 
 function ConfirmationModal({ cart, onButtonClick }) {
   return (
     <>
-      <div></div>
-      <dialog>
+      <div className={classes.backdrop}></div>
+      <dialog className={classes.modal}>
         <header>
-          <img src={confirmIcon} alt="Checkmark" />
-          <h2>Order Confirmed</h2>
-          <p>We hope you enjoy your food</p>
+          <img
+            src={confirmIcon}
+            alt="Checkmark"
+            className={classes.checkmark}
+          />
+          <h2 className={classes.title}>Order Confirmed</h2>
+          <p className={classes.text}>We hope you enjoy your food</p>
         </header>
         <section>
           <ul>
